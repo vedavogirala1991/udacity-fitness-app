4886a2b075781cb72cf313dc8440e936809610af
veda vogirala <veda.vogirala@gmail.com>
2021-02-26 02:52:16 +0530

    Udacity Fitness App - Adding Navigation doesnt really work

--- App.js
+++ App.js
@@ -3,15 +3,62 @@ import React,{Component} from 'react'
 import {View,
         Text,
         StyleSheet,
+        Platform,
         TouchableHighlight,
         TouchableOpacity,
         TouchableNativeFeedback,
         TouchableWithoutFeedback} from 'react-native'
 import AddEntry from './components/AddEntry'
 import History from './components/History'
+import {purple, white} from './utils/colors'
+import {createBottomTabNavigator, createMaterialTopTabNavigator} from 'react-navigation'
 import reducer from './reducers'
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+
+const RouteConfigs = {
+  History : {
+    screen : History,
+    navigationOptions : {
+      tabBarLabel : "History",
+      tabBarIcon : ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+    }
+  },
+  AddEntry : {
+    screen : AddEntry,
+    navigationOptions : {
+      tabBarLabel : "Add Entry",
+      tabBarIcon : ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+    }
+  }
+}
+
+const TabNavigatorConfig = {
+  navigationOptions : {
+    header : null
+  },
+  tabBarOptions: {
+    activeTintColor : Platform.OS === "ios" ? purple : white,
+    style : {
+      height : 56,
+      backgroundColor : Platform.OS === "ios" ? white : purple,
+      shadowColor : "rgba(0, 0, 0, 0.24)",
+      shadowOffset : {
+        width : 0,
+        height : 3
+      },
+      shadowRadius : 6,
+      shadowOpacity : 1
+    }
+  }
+}
+
+const Tabs = Platform.OS === "ios"
+  ? createBottomTabNavigator(RouteConfigs, TabNavigatorConfig)
+  : createMaterialTopTabNavigator(RouteConfigs, TabNavigatorConfig)
+
+
 
 class App extends Component {
   componentDidMount() {
@@ -23,7 +70,7 @@ class App extends Component {
     return (
       <Provider store={createStore(reducer)}>
         <View style={styles.container}>
-          <History />
+          <Tabs />
         </View>
       </Provider>
     )
