40520d8fdd6795e32933810ea0a1179e9c12c17a
veda vogirala <veda.vogirala@gmail.com>
2021-03-01 01:53:22 +0530

    Udacity Fitness App - GeoLocation methods

--- components/Live.js
+++ components/Live.js
@@ -1,11 +1,60 @@
 import React,{Component} from 'react'
-import {View, Text, ActivityIndicator } from 'react-native'
+import {View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from 'react-native'
+import * as Location from 'expo-location'
+import * as Permissions from 'expo-permissions'
+import {Foundation} from '@expo/vector-icons'
+import {purple, white} from '../utils/colors'
+import {calculateDirection} from '../utils/helpers'
 
 export default class Live extends Component {
   state= {
     coords : null,
     status : null,
-    direction : ''
+    direction : '',
+  }
+  componentDidMount () {
+    Permissions.getAsync(Permissions.LOCATION)
+      .then(({status})=> {
+        if(status==='granted') {
+          return this.setLocation()
+        }
+
+        this.setState(()=>({status}))
+      })
+      .catch((error)=> {
+        console.warn('Error getting Location permission : ',error)
+        this.setState(()=>({status : 'undetermined'}))
+      })
+  }
+  askPermission = () => {
+    Permissions.askAsync(Permissions.LOCATION)
+      .then(({status})=>{
+        if(status==='granted') {
+          return this.setLocation()
+        }
+
+        this.setState(()=>({status}))
+      })
+      .catch((error)=>{
+        console.warn('Error asking Location permission : ',error)
+      })
+  }
+
+  setLocation = () => {
+    Location.watchPositionAsync({
+      enableHighAccuracy : true,
+      timeInterval : 1,
+      distanceInterval : 1,
+    }, ({coords}) => {
+      const newDirection = calculateDirection(coords.heading)
+      const {direction} = this.state
+
+      this.setState (()=> ({
+        coords,
+        status : 'granted',
+        direction : newDirection,
+      }))
+    })
   }
 
   render() {
@@ -17,24 +66,120 @@ export default class Live extends Component {
 
     if(status === 'denied') {
       return (
-        <View>
-          <Text>Denied</Text>
+        <View style={styles.center}>
+          <Foundation name='alert' size={50}/>
+          <Text>
+            You denied your location. You can fix this by visiting your settings and enabling location services for this App.
+          </Text>
         </View>
       )
     }
 
     if(status === 'undetermined') {
       return (
-        <View>
-          <Text>Undetermined</Text>
+        <View style={styles.center}>
+          <Foundation name='alert' size={50}/>
+          <Text>
+            You need to enable location services for this App.
+          </Text>
+          <TouchableOpacity onPress={this.askPermission} style={styles.button}>
+            <Text style={styles.buttonText}>
+              Enable
+            </Text>
+          </TouchableOpacity>
         </View>
       )
     }
 
     return (
-      <View>
-        <Text>{JSON.stringify(this.state)}</Text>
+      <View style={styles.container}>
+        <View style={styles.directionContainer}>
+          <Text style={styles.directionHeader}>You're heading</Text>
+          <Text style={styles.direction}>{direction}</Text>
+        </View>
+        <View style={styles.metricContainer}>
+          <View style={styles.metric}>
+            <Text style={styles.header}>
+              Altitude
+            </Text>
+            <Text style={styles.subHeader}>
+              {Math.round(coords.altitude * 3.2808)} Feet
+            </Text>
+          </View>
+          <View style={styles.metric}>
+            <Text style={styles.header}>
+              Speed
+            </Text>
+            <Text style={styles.subHeader}>
+              {(coords.speed * 2.2369).toFixed(1)} MPH
+            </Text>
+          </View>
+        </View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create ({
+  container : {
+    flex : 1,
+    justifyContent : 'space-between'
+  },
+  center :  {
+    flex : 1,
+    justifyContent : 'center',
+    alignItems : 'center',
+    marginLeft : 30,
+    marginRight : 30,
+  },
+  button : {
+    padding : 10,
+    backgroundColor : purple,
+    alignSelf : 'center',
+    borderRadius : 5,
+    margin : 20,
+  },
+  buttonText : {
+    color : white,
+    fontSize : 20,
+  },
+  directionContainer : {
+    flex : 1,
+    justifyContent : 'center',
+  },
+  directionHeader  : {
+    fontSize : 35,
+    textAlign : 'center',
+  },
+  header  : {
+    fontSize : 35,
+    textAlign : 'center',
+    color : white,
+  },
+  direction : {
+    color : purple,
+    fontSize : 120,
+    textAlign : 'center',
+  },
+  metricContainer : {
+    flexDirection : 'row',
+    justifyContent : 'space-around',
+    backgroundColor : purple
+  },
+  metric : {
+    flex : 1,
+    paddingTop : 15,
+    paddingBottom : 15,
+    backgroundColor : 'rgba(255, 255, 255, 0.1)',
+    marginTop : 20,
+    marginBottom : 20,
+    marginLeft : 10,
+    marginRight : 10,
+  },
+  subHeader : {
+    fontSize : 25,
+    textAlign : 'center',
+    marginTop : 5,
+    color : white,
+  }
+})
