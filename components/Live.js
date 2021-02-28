ded78b2dd36b9d3d992a64b9167cb8df48b9748d
veda vogirala <veda.vogirala@gmail.com>
2021-03-01 02:39:21 +0530

    Udacity Fitness App - Direction Animation

--- components/Live.js
+++ components/Live.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react'
-import {View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from 'react-native'
+import {View, Text, ActivityIndicator, TouchableOpacity, StyleSheet, Animated, LogBox} from 'react-native'
 import * as Location from 'expo-location'
 import * as Permissions from 'expo-permissions'
 import {Foundation} from '@expo/vector-icons'
@@ -11,6 +11,7 @@ export default class Live extends Component {
     coords : null,
     status : null,
     direction : '',
+    bounceValue : new Animated.Value(1),
   }
   componentDidMount () {
     Permissions.getAsync(Permissions.LOCATION)
@@ -25,6 +26,7 @@ export default class Live extends Component {
         console.warn('Error getting Location permission : ',error)
         this.setState(()=>({status : 'undetermined'}))
       })
+    LogBox.ignoreLogs(['Animated: `useNativeDriver`'])
   }
   askPermission = () => {
     Permissions.askAsync(Permissions.LOCATION)
@@ -47,8 +49,14 @@ export default class Live extends Component {
       distanceInterval : 1,
     }, ({coords}) => {
       const newDirection = calculateDirection(coords.heading)
-      const {direction} = this.state
+      const {direction, bounceValue} = this.state
 
+      if(newDirection!==direction) {
+        Animated.sequence([
+          Animated.timing(bounceValue,{duration : 200, toValue : 1.04}),
+          Animated.spring(bounceValue,{toValue : 1, friction : 4}),
+        ]).start()
+      }
       this.setState (()=> ({
         coords,
         status : 'granted',
@@ -58,7 +66,7 @@ export default class Live extends Component {
   }
 
   render() {
-    const {status, coords, direction} = this.state
+    const {status, coords, direction, bounceValue} = this.state
 
     if(status === null){
       return <ActivityIndicator style={{marginTop : 30}}/>
@@ -95,7 +103,9 @@ export default class Live extends Component {
       <View style={styles.container}>
         <View style={styles.directionContainer}>
           <Text style={styles.directionHeader}>You're heading</Text>
-          <Text style={styles.direction}>{direction}</Text>
+          <Animated.Text style={[styles.direction, {transform : [{scale : bounceValue}]}]}>
+            {direction}
+          </Animated.Text>
         </View>
         <View style={styles.metricContainer}>
           <View style={styles.metric}>
